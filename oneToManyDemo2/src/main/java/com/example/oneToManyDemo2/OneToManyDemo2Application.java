e6ad5e331dd5e67e78346540591e86ca41f73091
NaralasettiHemaLatha <NaralasettiHemaLatha@LAPTOP-FIHO7033>
2022-12-19 16:05:54 +0530

    Added @EnableAutoConfiguration annotation

--- oneToManyDemo2/src/main/java/com/example/oneToManyDemo2/OneToManyDemo2Application.java
+++ oneToManyDemo2/src/main/java/com/example/oneToManyDemo2/OneToManyDemo2Application.java
@@ -1,9 +1,11 @@
 package com.example.oneToManyDemo2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class OneToManyDemo2Application {
 
 	public static void main(String[] args) {
